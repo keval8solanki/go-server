45f15819931dc80f012adf864c6c64a279a4c43f
Keval Solanki <keval8solanki@gmail.com>
2022-12-30 09:02:01 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+
+type Ping struct {
+  Message string `json:"message"`
+}
+
+func ping(ctx *gin.Context) {
+
+	var pingMessage = Ping{
+		Message: "pong",
+	}
+
+	ctx.IndentedJSON(http.StatusOK, pingMessage)
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", ping)
+
+	router.Run("localhost:9090")
+}
